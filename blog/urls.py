912f1c6567a39db32ec777b37364e57b147d2e9d
Aleksey Dzogiy <asdzog@mail.ru>
2023-12-19 09:18:41 +0400

    create urls for model Blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import BlogCreateView  # ContactsPageView, ProductDetailView
+from .views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 from blog.apps import BlogConfig
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='create'),
-    # path('', ..., name='view'),
-    # path('view/<int:pk>/', ..., name='view'),
-    # path('edit/<int:pk>/', ..., name='edit'),
-    # path('delete/<int:pk>/', ..., name='delete'),
+    path('', BlogListView.as_view(), name='list'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='view'),
+    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
 ]
