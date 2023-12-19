9a94775e1aae18de9512569c78d56204d35f6572
Aleksey Dzogiy <asdzog@mail.ru>
2023-12-19 09:22:21 +0400

    register Blog model in admin app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Blog
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'content', 'preview',
+                    'date_created', 'is_published', 'views_count', )
+    list_filter = ('is_published', )
+    search_fields = ('title', 'content', )
