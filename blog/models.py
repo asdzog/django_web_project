c266b84f31a56e343ba677a8e925d0da368ce9e6
Aleksey Dzogiy <asdzog@mail.ru>
2023-12-19 09:16:13 +0400

    edit model Blog

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ NULLABLE = {'blank': True, 'null': True}
 class Blog(models.Model):
     title = models.CharField(max_length=150, verbose_name='Пост')
     slug = models.CharField(max_length=200, verbose_name='Ссылка', **NULLABLE)
-    content = models.TextField(verbose_name='Содержание')
+    content = models.TextField(verbose_name='Содержание', **NULLABLE)
     preview = models.ImageField(upload_to='blog/', verbose_name='Превью')
     date_created = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     is_published = models.BooleanField(verbose_name='Опубликовано', default=True)
