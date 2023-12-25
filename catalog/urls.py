2d46cbabb8099f5c99ac89ba5bd602b4284d111b
asd <asdzog@mail.ru>
2023-12-25 17:33:52 +0400

    modify urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import HomeView, ContactsPageView, ProductDetailView
+from .views import HomeView, ContactsPageView, ProductDetailView, ProductCreateView
 
+app_name = 'catalog'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('contacts', ContactsPageView.as_view(), name='contacts'),
     path('product/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create_product/', ProductCreateView.as_view(), name='create_product'),
 
 ]
