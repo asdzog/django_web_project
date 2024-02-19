5950604cc300f99ac88b7622fd5d7b35c942ef75
asd <asdzog@mail.ru>
2024-02-19 16:53:41 +0400

    change Product creation form, fix form style

--- catalog/forms.py
+++ catalog/forms.py
@@ -7,14 +7,15 @@ class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            if field_name != 'is_active':
+                field.widget.attrs['class'] = 'form-control'
 
 
 class ProductForm(StyleFormMixin, forms.ModelForm):
 
     class Meta:
         model = Product
-        exclude = ('user', 'is_published')
+        exclude = ('owner', 'is_published')
 
     def clean_product_name(self):
         cleaned_data = self.cleaned_data['product_name']
