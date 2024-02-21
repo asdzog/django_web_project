0426e3393ad6450c2bf546e3240147aec5a2b578
asd <asdzog@mail.ru>
2024-02-21 17:56:54 +0400

    add cashing for product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from .views import HomeView, ContactsPageView, ProductDetailView, ProductCreateView, ProductUpdateView, \
     ProductDeleteView
 
@@ -7,7 +8,7 @@ app_name = 'catalog'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('contacts', ContactsPageView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('create_product/', ProductCreateView.as_view(), name='create_product'),
     path('update_product/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
     path('delete_product/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
